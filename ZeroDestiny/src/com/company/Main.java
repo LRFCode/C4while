a2212b9c898e7ed586aee80cefcb0866c6f97f25
Louis <lrflrf@sbcglobal.net>
2019-01-21 16:03:26 -0600

    Change variable name for clarity

--- ZeroDestiny/src/com/company/Main.java
+++ ZeroDestiny/src/com/company/Main.java
@@ -7,19 +7,19 @@ public class Main
         final int MAGIC_NUMBER = 0;
         java.util.Scanner in = new java.util.Scanner(System.in);
 
-        int numberOfGuesses = 0;
+        int numberOfIncorrectGuesses = 0;
 
         System.out.println("Please enter the magic whole number.");
         int userNumber = in.nextInt();
 
         while (userNumber != MAGIC_NUMBER)
         {
-            numberOfGuesses = numberOfGuesses + 1;
+            numberOfIncorrectGuesses = numberOfIncorrectGuesses + 1;
             System.out.println("FAIL!");
             System.out.println("Try again.");
             userNumber = in.nextInt();
         }
 
-        System.out.println("You entered a number other than zero " + numberOfGuesses + " times.");
+        System.out.println("You entered a number other than zero " + numberOfIncorrectGuesses + " times.");
     }
 }
